894642d64d0c581e8e4944c6fb8fb450d4b4b30b
Amine TEFFAL <a.teffal@gmail.com>
2020-10-18 21:06:07 +0100

    update

--- cars/src/App.js
+++ cars/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Mycars from './components/Mycars'
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <div className="App">
+        This is App component that include : 
+        <Mycars />
+      </div>
+    );
+
+  }
+
 }
 
 export default App;
---
+++ cars/src/components/Mycars.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Mycars extends Component {
+
+    render() {
+
+        return (
+
+            <div>
+
+                <h1>List of cars</h1>
+
+            </div>
+        )
+    }
+
+}
+
+export default Mycars;
